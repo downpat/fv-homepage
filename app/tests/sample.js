841a584b2dfc50aacc78c2e38faaa94be9b3d011
Patrick Dobson <pat@downpatproductions.com>
2017-01-06 19:08:40 -0500

    Changed test to make it pass again

--- app/tests/sample.js
+++ app/tests/sample.js
@@ -4,5 +4,5 @@ test('sample test', (t) => {
   t.plan(2); //We'll be running two assertions in this test
 
   t.equal(typeof Date.now, 'function');
-  t.equal('Pat', 'lame');
+  t.notEqual('Pat', 'lame');
 });
